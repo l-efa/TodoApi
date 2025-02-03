a754ba2a9003e2af3160c830dc84e53d350c4892
l-efa <lefa.sahi@gmail.com>
2025-02-03 20:29:57 +0200

    paketit ladattu. modelit ja controllerit lisätty

--- src/TodoApi/Program.cs
+++ src/TodoApi/Program.cs
@@ -1,13 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
